164225dec5aa794e6e85d2a5ba819f32b8e64fa2
Priyanshi Agarwal <147421491+priyanshiagarwal29@users.noreply.github.com>
2025-10-04 21:28:01 +0530

    Time: 0 ms (100%), Space: 43.4 MB (52.17%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -4,16 +4,20 @@ class Solution {
         while(left<=right){
             int mid=left+(right-left)/2;
             if(nums[mid]==target) return true;
-            if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
+            if (nums[left] == nums[mid]) {
                 left++;
+                continue;
+            }
+            if (nums[right] == nums[mid]) {
                 right--;
+                continue;
             } 
             else if(nums[left]<=nums[mid]){
                 if(nums[left]<=target && target<=nums[mid]) right=mid-1;
                 else left=mid+1;
             }
             else{
-                if(nums[mid]<target && target<nums[right]) left=mid+1;
+                if(nums[mid]<=target && target<=nums[right]) left=mid+1;
                 else right=mid-1;
             }
         }
