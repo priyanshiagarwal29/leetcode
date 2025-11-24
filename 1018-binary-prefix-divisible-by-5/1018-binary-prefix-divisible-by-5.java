b7bb87090473c91087b4662f1f157e6295f6be59
Priyanshi Agarwal <147421491+priyanshiagarwal29@users.noreply.github.com>
2025-11-24 19:24:57 +0530

    Time: 3 ms (99.2%), Space: 47.8 MB (6.57%) - LeetHub

---
+++ 1018-binary-prefix-divisible-by-5/1018-binary-prefix-divisible-by-5.java
@@ -0,0 +1,11 @@
+class Solution {
+    public List<Boolean> prefixesDivBy5(int[] nums) {
+        List<Boolean> ans=new ArrayList<>();
+        int val=0;
+        for(int n:nums){
+            val=((val*2)+n)%5;
+            ans.add(val==0);
+        }
+        return ans;
+    }
+}
