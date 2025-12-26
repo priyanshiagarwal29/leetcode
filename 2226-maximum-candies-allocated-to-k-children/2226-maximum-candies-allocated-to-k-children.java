8f6886bed2ca76604dab14937071e5eb6b72e158
Priyanshi Agarwal <147421491+priyanshiagarwal29@users.noreply.github.com>
2025-12-26 14:47:48 +0530

    Time: 23 ms (94.1%), Space: 91.8 MB (72.97%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
@@ -1,23 +1,30 @@
 class Solution {
     public int maximumCandies(int[] candies, long k) {
-        int total=0,h=0,ans=0;
-        for(int n:candies) 
-        {
-            total+=n;
-             h = Math.max(n,h);
+        long total = 0;
+        int h = 0;
+
+        for (int n : candies) {
+            total += n;          
+            h = Math.max(h, n);
         }
-        if(total<k) return 0;
-        int l=1;
-        while(l<=h){
-            int mid=l+(h-l)/2;
-            int c=0;
-            for(int cn:candies) c+=cn/mid;
-            if(c>=k){
-                ans=mid;
-                l=mid+1;
+
+        if (total < k) return 0;
+
+        int l = 1, ans = 0;
+
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            long c = 0;          
+
+            for (int cn : candies) {
+                c += cn / mid;
             }
-            else {
-                h=mid-1;
+
+            if (c >= k) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                h = mid - 1;
             }
         }
         return ans;
