815c2b460e69e7f9fd3fdb5e599974144e1c249e
Priyanshi Agarwal <147421491+priyanshiagarwal29@users.noreply.github.com>
2026-02-03 19:51:32 +0530

    Time: 1 ms (29.61%), Space: 47.4 MB (43.4%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,13 +1,20 @@
 class Solution {
+    public void swap(int i,int j,int[] arr){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
     public int missingNumber(int[] nums) {
-        HashSet<Integer> set=new HashSet<>();
-        for(int num:nums){
-            set.add(num);
-        }
         int i=0;
-       while(set.contains(i)){
-        i++;
-       }
-       return i;
+        while(i<nums.length){
+            if(nums[i]<nums.length && nums[i] !=nums[nums[i]]) swap(i,nums[i],nums);
+            else{
+                i++;
+            }
+        }
+        for(i=0;i<nums.length;i++){
+            if(nums[i] !=i) return i;
+        }
+        return nums.length;
     }
 }
