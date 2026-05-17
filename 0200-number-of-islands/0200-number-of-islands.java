eca92752a0efb651a7b03c6dc823679a112c7749
Priyanshi Agarwal <147421491+priyanshiagarwal29@users.noreply.github.com>
2026-05-17 13:28:33 +0530

    Time: 6 ms (20.23%), Space: 51.3 MB (92.61%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,55 +1,58 @@
-class Pair{
-    int first;
-    int sec;
-    public Pair(int i,int j){
-        this.first=i;
-        this.sec=j;
-    }
-}
-
 class Solution {
-
-    public static void bfs(char[][] grid,boolean[][] vis,int i,int j){
-        vis[i][j]=true;
-        Queue<Pair> q=new LinkedList<Pair>();
-        q.add(new Pair(i,j));
-        while(!q.isEmpty()){
-            int r=q.peek().first;
-            int c=q.peek().sec;
-            q.remove();
-            if(c-1>=0 && vis[r][c-1]== false && grid[r][c-1]=='1') {
-                q.add(new Pair(r,c-1));
-                vis[r][c-1]=true;
-            }
-             if(c+1<grid[0].length && vis[r][c+1]== false && grid[r][c+1]=='1') {
-                q.add(new Pair(r,c+1));
-                vis[r][c+1]=true;
-            }
-             if(r-1>=0 && vis[r-1][c]== false && grid[r-1][c]=='1') {
-                q.add(new Pair(r-1,c));
-                vis[r-1][c]=true;
-            }
-            if(r+1<grid.length && vis[r+1][c]== false && grid[r+1][c]=='1') {
-                q.add(new Pair(r+1,c));
-                vis[r+1][c]=true;
-            }
+    class pair{
+        int i;
+        int j;
+        public pair(int i,int j){
+            this.i=i;
+            this.j=j;
         }
     }
 
+    public void  bfs(char[][] grid,boolean[][] vis,int i,int j){
+       vis[i][j]=true;
+       Queue<pair> q=new LinkedList<>();
+       q.add(new pair(i,j));
+
+       while(!q.isEmpty()){
+        int r=q.peek().i;
+        int c=q.peek().j;
+        q.remove();
+
+        if(c-1>=0 && vis[r][c-1]==false && grid[r][c-1]=='1'){
+            q.add(new pair(r,c-1));
+            vis[r][c-1]=true;
+        }
+        
+        if(c+1<grid[0].length && vis[r][c+1]==false && grid[r][c+1]=='1'){
+            q.add(new pair(r,c+1));
+            vis[r][c+1]=true;
+        }
+
+        if(r-1>=0 && vis[r-1][c]==false && grid[r-1][c]=='1'){
+            q.add(new pair(r-1,c));
+            vis[r-1][c]=true;
+        }
+
+        if(r+1<grid.length && vis[r+1][c]==false && grid[r+1][c]=='1'){
+            q.add(new pair(r+1,c));
+            vis[r+1][c]=true;
+        }
+       }
+    }
     public int numIslands(char[][] grid) {
-        int n=grid.length;
-        int m=grid[0].length;
-        boolean[][] vis=new boolean[n][m];
+        int m=grid.length;
+        int n=grid[0].length;
+
+        boolean[][] vis=new boolean[m][n];
+        
         int cnt=0;
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-              if(grid[i][j]=='1'){
-                if(vis[i][j]==false){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]=='1' && vis[i][j]!=true){
                     cnt++;
                     bfs(grid,vis,i,j);
                 }
-              }
             }
         }
         return cnt;
