23a8efd57d760b37ed0c3984568ab5161ecd24bf
Priyanshi Agarwal <147421491+priyanshiagarwal29@users.noreply.github.com>
2026-05-19 23:08:22 +0530

    Time: 5 ms (82.64%), Space: 77.3 MB (47.38%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,15 +1,17 @@
 class Solution {
     public int maxArea(int[] height) {
-        // we need to move that pointer forward whose height is minimum becoz if we did the other one the minimun will still be small
-        int i=0;int j=height.length-1;
-        int max=0;
+        int i=0,j=height.length-1,area=0;
+        int ans=0;
+        
         while(i<j){
-          int s=Math.min(height[i],height[j]);
-          int area=s*(j-i);
-          max=Math.max(max,area);
-          if(height[i]<=height[j]) i++;
-          else if(height[i]>height[j]) j--;
+        int b=Math.min(height[i],height[j]);
+        area=b*(j-i);
+        ans=Math.max(area,ans);
+        if(height[i]<=height[j]) i++;
+        else{
+          j--;
         }
-        return max;
+        }
+        return ans;
     }
 }
